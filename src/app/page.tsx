0685d2cba459a53bff927a47c206013187458cf0
azimuddin2 <mohammadazimuddin274@gmail.com>
2026-09-19 12:31:54 +0600

    issue fix

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="bg-green-500 p-3 rounded font-semibold">Intentional Fitness Admin Panel</h1>
-    </div>
-  );
-}
